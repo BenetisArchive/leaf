cd078c504791702cddf74d7fdabd0dd045ce4832
SimSte <simonas.stepanovas@gmail.com>
2014-05-23 00:53:37 +0300

    Merged

--- leaf.c
+++ leaf.c
@@ -14,17 +14,19 @@
 #include <stdlib.h>
 
 void *getInfo(void *ptr);
+void catchInt(int interrupt);
 
 int main (int argc, char const *argv[])
 {
   struct sockaddr_in server_address;
   int socketfd = socket(AF_INET, SOCK_STREAM, 0);
   memset(&server_address, '0', sizeof(server_address));
+  signal(SIGINT, catchInt);
 
   server_address.sin_family = AF_INET;
   server_address.sin_addr.s_addr = htonl(INADDR_ANY);
   server_address.sin_port = htons(1337);
-  //when socket is create, it does not have address assigned
+  //when socket is created, it does not have address assigned
   //bind assigns address to socket
   bind(socketfd, (struct sockaddr*) &server_address, sizeof(server_address));
 
@@ -40,7 +42,7 @@ int main (int argc, char const *argv[])
     connfd = accept(socketfd,(struct sockaddr*)NULL, NULL);
     if(connfd == -1) perror("Could not accept socket");
     pid_t pid = fork();
-    if (!pid) {
+    if (pid != 0) {
       close(connfd);
     } else {
       write(connfd, "HTTP/1.1 200 OK\n", 16);
@@ -55,7 +57,6 @@ int main (int argc, char const *argv[])
         return 2;
       }
       response = (char *)return_value;
-
       sprintf(contentLength, "Content-length: %d\n", (int)strlen(response));
       write(connfd, contentLength, strlen(contentLength));
       write(connfd, "Content-Type: text/html\n\n", 25);
@@ -76,13 +77,16 @@ void *getInfo(void *ptr) {
   size_t len = 0;
   ssize_t read;
   int written = 0;
-   if (file == NULL)
-       exit(EXIT_FAILURE);
-    char * line = NULL;
-   while ((read = getline(&line, &len, file)) != -1) {
-       strcat(buffer, line);
-       strcat(buffer, "<br />");
-   }
-
+  if (file == NULL)
+	exit(EXIT_FAILURE);
+  char * line = NULL;
+  while ((read = getline(&line, &len, file)) != -1) {
+	strcat(buffer, line);
+	strcat(buffer, "<br />");
+  }
   pthread_exit((void *)buffer);
+}
+
+void catchInt(int interrupt) {
+	exit(EXIT_SUCCESS);
 }
